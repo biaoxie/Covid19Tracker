c3f565f2e2ab17431116b88fd5827b93e0c2fd0c
biaoxie <biao.xie001@gmail.com>
2021-08-26 15:48:47 -0400

    supports latest info for specific state

--- src/main/java/com/biaoxie/CovidTracker/controllers/CovidController.java
+++ src/main/java/com/biaoxie/CovidTracker/controllers/CovidController.java
@@ -1,11 +1,15 @@
 package com.biaoxie.CovidTracker.controllers;
 
+import com.biaoxie.CovidTracker.models.CaseInfo;
 import com.biaoxie.CovidTracker.models.StateEntity;
 import com.biaoxie.CovidTracker.services.CovidDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CovidController {
@@ -14,8 +18,10 @@ public class CovidController {
     private CovidDataService covidDataService;
 
     @GetMapping("/{fips}")
-    public StateEntity findStateByFips(@PathVariable String fips) {
-        return covidDataService.findStateByFips(Integer.parseInt(fips));
+    public List<CaseInfo> findLatestStateInfoByFips(@PathVariable String fips) {
+        StateEntity stateEntity = covidDataService.findStateByFips(Integer.parseInt(fips));
+        return stateEntity.getCaseInfos().subList(stateEntity.getCaseInfos().size()-10, stateEntity.getCaseInfos().size());
     }
+
 
 }
--- src/main/java/com/biaoxie/CovidTracker/controllers/HomeController.java
+++ src/main/java/com/biaoxie/CovidTracker/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package com.biaoxie.CovidTracker.controllers;
 
 import com.biaoxie.CovidTracker.data.CovidRepository;
+import com.biaoxie.CovidTracker.models.CaseInfo;
 import com.biaoxie.CovidTracker.models.StateEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Controller
 public class HomeController {
@@ -24,5 +28,46 @@ public class HomeController {
         model.addAttribute("allStates", allStates);
 
         return "home";
+    }
+
+    @GetMapping("/{fips}/latest")
+    public String statePage(Model model, @PathVariable String fips) {
+
+        StateEntity stateEntity = null;
+        if (Pattern.matches("[0-9]+", fips)) {
+            stateEntity = covidRepository.findByFips(Integer.parseInt(fips));
+        }
+
+        if (stateEntity != null) {
+            model.addAttribute("stateName", stateEntity.getStateName());
+            List<CaseInfo> caseInfos = stateEntity.getCaseInfos();
+            caseInfos = caseInfos.subList(caseInfos.size()-11, caseInfos.size());
+            model.addAttribute("totalCases", caseInfos.get(caseInfos.size()-1).getCases());
+
+            List<CaseInfo> newCases = new ArrayList<>(caseInfos);
+            for(int i = newCases.size() - 1; i > 0; i--) {
+                CaseInfo newCase = new CaseInfo();
+                CaseInfo current = newCases.get(i);
+                CaseInfo past = newCases.get(i-1);
+                newCase.setCases(current.getCases()-past.getCases());
+                newCase.setDeaths(current.getDeaths()-past.getDeaths());
+                newCases.set(i, newCase);
+            }
+
+            newCases.remove(0);
+            caseInfos.remove(0);
+
+            model.addAttribute("caseInfos", caseInfos);
+            model.addAttribute("newCases", newCases);
+
+
+
+
+
+        }
+
+
+
+        return "state";
     }
 }
