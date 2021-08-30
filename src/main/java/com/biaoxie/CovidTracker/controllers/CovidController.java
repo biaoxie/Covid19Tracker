9d42226d79013df9e580690ea8753a7dc14ce0af
biaoxie <biao.xie001@gmail.com>
2021-08-30 14:18:27 -0400

    added thousand separator for cases and deaths.

--- src/main/java/com/biaoxie/CovidTracker/controllers/CovidController.java
+++ src/main/java/com/biaoxie/CovidTracker/controllers/CovidController.java
@@ -4,8 +4,7 @@ import com.biaoxie.CovidTracker.models.CaseInfo;
 import com.biaoxie.CovidTracker.models.StateEntity;
 import com.biaoxie.CovidTracker.services.CovidDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +24,6 @@ public class CovidController {
             StateEntity stateEntity = covidDataService.findStateByFips(Integer.parseInt(fips));
             return stateEntity.getCaseInfos().subList(stateEntity.getCaseInfos().size() - 10, stateEntity.getCaseInfos().size());
         }
-
         return null;
     }
 
